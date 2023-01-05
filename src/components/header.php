2f8b269f843233e17b4f47545dc3aba938c7c575
DorkArn02 <dorkproarn02@gmail.com>
2023-01-05 16:41:58 +0100

    header cleaned

--- src/components/header.php
+++ src/components/header.php
@@ -1,7 +1,3 @@
 <header>
     <?php include("navigation.php") ?>
-    <div class="header">
-        <h2>Lorem lipsum dolor sit amet</h2>
-        <p>Anim aute ex nulla dolor voluptate nostrud tempor est commodo. Minim veniam incididunt ad mollit non commodo culpa labore est. Commodo laboris qui sit occaecat cillum duis ea voluptate. Dolor eu officia sit consequat eu incididunt cillum eiusmod id elit ea aute.</p>
-    </div>
 </header>
